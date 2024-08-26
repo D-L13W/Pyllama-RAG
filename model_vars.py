71a0da3a7b4994ae7bec25cdea1426d79b25133c
D-L13W <damian.liewjx@gmail.com>
2024-08-26 18:26:52 +0800

    Played around with some model variables and language models available from Ollama. Limited sources per query to 6 instead of 10.

--- model_vars.py
+++ model_vars.py
@@ -5,7 +5,7 @@ from langchain_community.llms.ollama import Ollama
 EMBEDDING_MODEL_FUNCTION = OllamaEmbeddings(
     model="bge-m3"
 )  # Manually specify your model please
-LANGUAGE_MODEL_FUNCTION = Ollama(model="llama3.1:latest")
+LANGUAGE_MODEL_FUNCTION = Ollama(model="phi3:14b-medium-4k-instruct-q4_0")
 
 # Query variables
 PROMPT_TEMPLATE: str = """
@@ -18,4 +18,4 @@ Answer the question based only on the following context:
 Answer the following question based only on the above context: {question}
 """
 ## How many chunks/sources to take into account when answering a query
-NUM_SOURCES: int = 10
+NUM_SOURCES: int = 6
